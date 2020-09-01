a69e18cf72e9a75026cc9d10c38b6d645bf16617
aganek9611 <aganek9611@gmail.com>
2020-09-01 23:34:11 +0300

    Add user profiles, add avatars for user, handling errors

--- config.py
+++ config.py
@@ -6,5 +6,15 @@ basedir = Path(__file__).parent
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # sql_alchemy_config
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # email_config
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['aganek9611@gmail.com']
